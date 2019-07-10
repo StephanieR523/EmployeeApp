3173466ba675e07a947794adcbd1b8c2a65a0949
mavstronaut <mavbarona@gmail.com>
2019-07-09 20:42:40 -0400

    fixing firebase link

--- assets/javascript/firebaseconfig.js
+++ assets/javascript/firebaseconfig.js
@@ -11,3 +11,56 @@
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
+
+
+  // Create a variable to reference the database.
+  var database = firebase.database();
+
+  // Initial Values
+  var name = "";
+  var role = "";
+  var start = 0;
+  var end = "";
+
+  // Capture Button Click
+  $("#add-user").on("click", function(event) {
+    event.preventDefault();
+
+    // Grabbed values from text boxes
+    name = $("#employee-name").val().trim();
+    role = $("#role").val().trim();
+    start = $("#start-year").val().trim();
+    end = $("#end-year").val().trim();
+
+    // Code for handling the push
+    database.ref().push({
+      name: name,
+      role: role,
+      start: start,
+      end: end,
+      dateAdded: firebase.database.ServerValue.TIMESTAMP
+    });
+
+  });
+
+  // Firebase watcher .on("child_added"
+  database.ref().on("child_added", function(snapshot) {
+    // storing the snapshot.val() in a variable for convenience
+    var sv = snapshot.val();
+
+    // Console.loging the last user's data
+    console.log(sv.name);
+    console.log(sv.role);
+    console.log(sv.start);
+    console.log(sv.end);
+
+    // Change the HTML to reflect
+    $("#name-display").text(sv.name);
+    $("#email-display").text(sv.role);
+    $("#age-display").text(sv.start);
+    $("#comment-display").text(sv.end);
+
+    // Handle the errors
+  }, function(errorObject) {
+    console.log("Errors handled: " + errorObject.code);
+  });
